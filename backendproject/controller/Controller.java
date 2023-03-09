996a1134377bba94d2eab5119cc1fc7ba5c1bf9d
pavi-28 <91819454+pavi-28@users.noreply.github.com>
2023-03-09 21:25:46 +0530

    Update Controller.java

--- backendproject/controller/Controller.java
+++ backendproject/controller/Controller.java
@@ -38,13 +38,20 @@ public class Controller {
 	public List<Student> getAllStudents(){
 		return repository.findAll();
 	}		
-	
+	/* By using post mapping annotation, transfer data from client to server in HTTP protocol.
+         * POST carries request parameter in message body which makes it more secure
+         * way of transferring data from client to server.*/
+
 	// create Student rest api
 	@PostMapping("/Students")
 	public Student createStudent(@RequestBody Student student) {
 		return repository.save(student);
 	}
 	
+       /* By using get mapping annotation, transfer data from client to server in HTTP protocol.
+        * It is used to get a single or multiple resources
+        * It carries request parameter appended in URL string */
+
 	// get Student by id rest api
 	@GetMapping("/Students/{id}")
 	public ResponseEntity<Student> getStudentById(@PathVariable Integer id) {
@@ -52,7 +59,8 @@ public class Controller {
 				.orElseThrow(() -> new ResourceNotFound("Student not exist with id :" + id));
 		return ResponseEntity.ok(student);
 	}
-	
+	/* This method is used to update/modify the resource 
+	 * so @PutMapping annotation is used for mapping HTTP PUT requests onto specific handler methods.*/
 	// update Student rest api
 	
 	@PutMapping("/Students/{id}")
@@ -67,7 +75,9 @@ public class Controller {
 		Student updatedStudent = repository.save(student);
 		return ResponseEntity.ok(updatedStudent);
 	}
-	
+	/* The Delete HTTP method is used to delete the resource and @DeleteMapping annotation for mapping 
+         * HTTP DELETE requests onto specific handler methods.*/
+
 	// delete Student rest api
 	@DeleteMapping("/Students/{id}")
 	public ResponseEntity<Map<String, Boolean>> deleteStudent(@PathVariable Integer id){
