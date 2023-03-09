d8df8a70878bc9aa6ecac6b77faff3515bafa342
pavi-28 <91819454+pavi-28@users.noreply.github.com>
2023-03-09 21:20:19 +0530

    Update Sturepository.java

--- backendproject/repository/Sturepository.java
+++ backendproject/repository/Sturepository.java
@@ -3,7 +3,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.backendproject.model.Student;
-
+//This Interface extends JpaRepository interface, so that @Repository is not needed to be annotated manually
 @Repository
 public interface Sturepository extends JpaRepository<Student, Integer>{
 
